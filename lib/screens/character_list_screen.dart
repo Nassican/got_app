e95476f0d94a12862599670127e255bd8fb0a4ed
Jesús Benavides <nassicand@gmail.com>
2024-11-27 07:45:50 -0500

    Add about dialog to show app information

--- lib/screens/character_list_screen.dart
+++ lib/screens/character_list_screen.dart
@@ -21,11 +21,65 @@ class _CharacterListScreenState extends State<CharacterListScreen> {
     _characters = _apiService.getCharacters();
   }
 
+  void _showAboutDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          title: const Row(
+            children: [
+              Icon(Icons.info_outline, color: Colors.grey),
+              SizedBox(width: 10),
+              Text('Acerca de'),
+            ],
+          ),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Game of Thrones App',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Versión: 1.0.0'),
+              SizedBox(height: 5),
+              Text('Desarrollado con Flutter'),
+              SizedBox(height: 5),
+              Text('API: thronesapi.com'),
+              SizedBox(height: 5),
+              Text('Developed By: Jesús Benavides')
+
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cerrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Game of Thrones'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info_outline),
+            onPressed: _showAboutDialog,
+          ),
+        ],
       ),
       body: FutureBuilder<List<Character>>(
         future: _characters,
